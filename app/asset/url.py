820b2bc08d17c32658e3debf7dcac1cf963a3a5e
MackeiWeng <584558424@qq.com>
2018-09-25 20:47:49 +0800

    update

--- app/asset/url.py
+++ app/asset/url.py
@@ -7,7 +7,7 @@ from .view import AssetCloudRoom,AssetCloudHost
 def get_asset_resources():
     auth_bp = Blueprint('asset', __name__, template_folder='../../templates', static_url_path='', static_folder='')
     api = Api(auth_bp)
-    api.add_resource(AssetCloudRoom, '/cloudhost')
+    api.add_resource(AssetCloudRoom, '/cloudroom')
     api.add_resource(AssetCloudHost, '/cloudhost')
     return auth_bp
 
--- app/asset/view.py
+++ app/asset/view.py
@@ -2,13 +2,14 @@
 from flask_restful import Resource, reqparse, request
 from .model import CloudHost,CloudRoom
 from common.common import falseReturn,trueReturn
+import  logging
 
 class AssetCloudRoom(Resource):
 
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument("limit",type=int)
-        parser.add_argument("offser",type=int)
+        parser.add_argument("offset",type=int)
         parser.add_argument("name",type=str)
         parser.add_argument("id",type=int)
         args = parser.parse_args()
@@ -18,7 +19,38 @@ class AssetCloudRoom(Resource):
         if args.get("id"):
             filter_list.append(CloudRoom.id == args.get("id"))
         print(filter_list)
-        return  trueReturn(data="{}",msg="ture")
+        try:
+            query = CloudRoom.query.filter_by(*filter_list).order_by(CloudHost.id)
+            total = query.count()
+            print(total)
+            limit = args.get("limit",10)
+            offset = args.get("offset",1)
+            # query = query.offset(int((offset - 1) * limit))
+            # result = query.limit(limit)
+            result = query.paginate(offset,limit,False)
+            print(result)
+            return  trueReturn(data=result,msg="ture")
+        except Exception as e:
+            msg = e
+            return falseReturn(data="",msg=msg)
+
+    def post(self):
+        pass
+
+
+    def _is_exit(self,name):
+        try:
+            result = CloudRoom.query.filter_by(name=name).count()
+            if result > 0:
+                return  False
+            else:
+                return  True
+        except Exception as e:
+            print(e)
+
+
+
+
 
 
 class AssetCloudHost(Resource):
