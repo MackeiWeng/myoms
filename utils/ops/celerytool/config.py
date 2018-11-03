6f917cc4a4bf5ee602cdd3651ed855a66a584bb7
MackeiWeng <584558424@qq.com>
2018-11-03 16:24:19 +0800

    update

--- utils/ops/celerytool/config.py
+++ utils/ops/celerytool/config.py
@@ -6,4 +6,13 @@ CELERY_RESULT_BACKEND = 'redis://127.0.0.1:7001/0'  # 指定 Backend
 CELERY_TIMEZONE='Asia/Shanghai'                     # 指定时区，默认是 UTC
 CELERYD_MAX_TASKS_PER_CHILD = 40     # 每个worker执行了多少任务就会死掉,防止内存泄露
 
+CELERY_ROUTES = {
+    'utils.ops.celerytool.ansible_task.tasks.run_fastscripts': {'queue': 'run_fastscripts'},
+    # 'task_celery.task_worker_2': {'queue': 'task_worker_2'},
+}
+
+
+CELERY_IMPORTS = (
+    'utils.ops.celerytool.ansible_task.tasks',
+)
 
--- utils/ops/celerytool/main.py
+++ utils/ops/celerytool/main.py
@@ -1,7 +1,11 @@
 # coding:utf-8
 
+import psutil
 from celery import  Celery
 from utils.ops.celerytool import config
 celery_app = Celery("my_tasks")
 celery_app.config_from_object(config)
-celery_app.autodiscover_tasks(["utils.ops.celerytool.ansible_task.ansible_task"])
+celery_app.autodiscover_tasks(["utils.ops.celerytool.ansible_task"])
+
+psutil.disk_io_counters()
+
